2cbc30d50208a15b126f25ccd27fef2c79f6c237
shifu-kobby <samuelarmahappiah@gmail.com>
2023-01-21 22:07:08 +0000

    created formfield compnent

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { FormField, Loader } from '../components';
+import { getRandomPrompt } from '../utils';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: ''
+  })
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+          Create imaginative and visually stunning images through DALL-E and share them with the community
+        </p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Sam Appiah'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName='Your Prompt'
+            type='text'
+            name='prompt'
+            placeholder='a painting of a fox in the style of Starry Night'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
